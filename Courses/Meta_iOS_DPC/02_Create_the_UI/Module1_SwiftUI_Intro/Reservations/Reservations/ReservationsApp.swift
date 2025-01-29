6f21514a0ad21dde41f6b0d9c46e131ba384799e
PabloRR100 <pabloruizruiz10@gmail.com>
2025-01-29 18:52:49 +0100

    Created first draft project in SwiftUI

---
+++ Courses/Meta_iOS_DPC/02_Create_the_UI/Module1_SwiftUI_Intro/Reservations/Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Pablo Ruiz Ruiz on 29/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
