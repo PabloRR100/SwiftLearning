9eb01292ef015f7afb3ee981433c7343f07752d5
PabloRR100 <pabloruizruiz10@gmail.com>
2025-02-02 18:49:06 +0100

    Module 2 - Unit 1 - Views

---
+++ Courses/Meta_iOS_DPC/02_Create_the_UI/Module2_SwiftUI_Fundamentals/Stacks/Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Pablo Ruiz Ruiz on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
